Include micro average Fix plot notebook to accomodate micro average

src/cafaeval/evaluation.py
@@ -24,10 +24,13 @@ def compute_s(ru, mi):
     # return np.where(np.isnan(ru), mi, np.sqrt(ru + np.nan_to_num(mi)))
 
 
-def compute_metrics_(tau_arr, g, pred, toi, n_gt, ic_arr):
+def compute_metrics_(tau_arr, g, pred, toi, n_gt, ic_arr=None):
+    """
+    Perform the evaluation at the matrix level for all tau thresholds
+    The calculation is
+    """
 
-    # cov, tp, fp, fn, pr, rc
-    # fp = misinformation, fn = remaining uncertainty)
+    # n, tp, fp, fn, pr, rc (fp = misinformation, fn = remaining uncertainty)
     metrics = np.zeros((len(tau_arr), 6), dtype='float')
 
     for i, tau in enumerate(tau_arr):
@@ -35,7 +38,7 @@ def compute_metrics_(tau_arr, g, pred, toi, n_gt, ic_arr):
         # Filter predictions based on tau threshold
         p = solidify_prediction(pred.matrix[:, toi], tau)
 
-        # Coverage, number of proteins with at least one term predicted with score >= tau
+        # Number of proteins with at least one term predicted with score >= tau
         metrics[i, 0] = (p.sum(axis=1) > 0).sum()
 
         # Terms subsets
@@ -63,11 +66,9 @@ def compute_metrics_(tau_arr, g, pred, toi, n_gt, ic_arr):
             metrics[i, 2] = (mis * ic_arr[toi]).sum(axis=1).sum()
             metrics[i, 3] = (remaining * ic_arr[toi]).sum(axis=1).sum()
 
-        # Precision, recall (micro-average)
-        metrics[i, 4] = np.divide(n_intersection, n_pred, out=np.zeros_like(n_intersection, dtype='float'),
-                                  where=n_pred > 0).sum()
-        metrics[i, 5] = np.divide(n_intersection, n_gt, out=np.zeros_like(n_gt, dtype='float'),
-                                  where=n_gt > 0).sum()
+        # Precision, recall
+        metrics[i, 4] = np.divide(n_intersection, n_pred, out=np.zeros_like(n_intersection, dtype='float'), where=n_pred > 0).sum()
+        metrics[i, 5] = np.divide(n_intersection, n_gt, out=np.zeros_like(n_gt, dtype='float'), where=n_gt > 0).sum()
 
     return metrics
 
@@ -139,23 +140,39 @@ def evaluate_prediction(prediction, gt, ontologies, tau_arr, normalization='cafa
         metrics['cov'] = np.divide(metrics['n'], ne, out=np.zeros_like(metrics['n'], dtype='float'), where=ne > 0)
         metrics['f'] = compute_f(metrics['pr'], metrics['rc'])
 
+        # Micro average
+        metrics['pr_micro'] = np.divide(metrics['tp'], metrics['tp'] + metrics['fp'],
+                                        out=np.zeros_like(metrics['tp'], dtype='float'),
+                                        where=(metrics['tp'] + metrics['fp']) > 0)
+        metrics['rc_micro'] = np.divide(metrics['tp'], metrics['tp'] + metrics['fn'],
+                                        out=np.zeros_like(metrics['tp'], dtype='float'),
+                                        where=(metrics['tp'] + metrics['fn']) > 0)
+        metrics['f_micro'] = compute_f(metrics['pr_micro'], metrics['rc_micro'])
+
         if ontologies[ns].ia is not None:
             ne = np.full(len(tau_arr), gt[ns].matrix[:, ontologies[ns].toi_ia].shape[0])
 
             metrics['wcov'] = np.divide(metrics['wn'], ne, out=np.zeros_like(metrics['wn'], dtype='float'), where=ne > 0)
             metrics['wf'] = compute_f(metrics['wpr'], metrics['wrc'])
             metrics['mi'] = np.divide(metrics['wfp'], ne, out=np.zeros_like(metrics['wcov'], dtype='float'), where=ne > 0)
-            metrics['ru'] = np.divide(metrics['wfn'], ne, out=np.zeros_like(metrics['wcov'], dtype='float'),
-                                      where=ne > 0)
+            metrics['ru'] = np.divide(metrics['wfn'], ne, out=np.zeros_like(metrics['wcov'], dtype='float'), where=ne > 0)
             metrics['s'] = compute_s(metrics['ru'], metrics['mi'])
+
+            # Micro average
+            metrics['wpr_micro'] = np.divide(metrics['wtp'], metrics['wtp'] + metrics['wfp'],
+                                              out=np.zeros_like(metrics['wtp'], dtype='float'),
+                                              where=(metrics['wtp'] + metrics['wfp']) > 0)
+            metrics['wrc_micro'] = np.divide(metrics['wtp'], metrics['wtp'] + metrics['wfn'],
+                                              out=np.zeros_like(metrics['wtp'], dtype='float'),
+                                              where=(metrics['wtp'] + metrics['wfn']) > 0)
+            metrics['wf_micro'] = compute_f(metrics['wpr_micro'], metrics['wrc_micro'])
 
         dfs.append(metrics)
 
     return pd.concat(dfs)
 
 
-def cafa_eval(obo_file, pred_dir, gt_file, ia=None, no_orphans=False, norm='cafa', prop='max',
-              max_terms=None, th_step=0.01, threads=1):
+def cafa_eval(obo_file, pred_dir, gt_file, ia=None, no_orphans=False, norm='cafa', prop='max', max_terms=None, th_step=0.01, threads=1):
 
     # Tau array, used to compute metrics at different score thresholds
     tau_arr = np.arange(th_step, 1, th_step)
@@ -196,21 +213,14 @@ def cafa_eval(obo_file, pred_dir, gt_file, ia=None, no_orphans=False, norm='cafa
         df = df[df['cov'] > 0].reset_index(drop=True)
         df.set_index(['filename', 'ns', 'tau'], inplace=True)
 
-        # Select columns to save
-        if ia is not None:
-            columns = ["n", "tp", "fp", "fn", "cov", "pr", "rc", "f", "wn", "wtp", "wfp", "wfn", "wcov", "wpr", "wrc", "wf", "mi", "ru", "s"]
-        else:
-            columns = ["n", "tp", "fp", "fn", "cov", "pr", "rc", "f"]
-        df = df[columns]
-
         # Calculate the best index for each namespace and each evaluation metric
-        for metric, cols in [('f', ['rc', 'pr']), ('wf', ['wrc', 'wpr']), ('s', ['fn', 'fp'])]:
-            if metric in columns:
-                index_best = df.groupby(level=['filename', 'ns'])[metric].idxmax() if metric in ['f', 'wf'] else \
-                    df.groupby(['filename', 'ns'])[metric].idxmin()
+        for metric, cols in [('f', ['rc', 'pr']), ('wf', ['wrc', 'wpr']), ('s', ['ru', 'mi']), ('f_micro', ['rc_micro', 'pr_micro']), ('wf_micro', ['wrc_micro', 'wpr_micro'])]:
+            if metric in df.columns:
+                index_best = df.groupby(level=['filename', 'ns'])[metric].idxmax() if metric in ['f', 'wf', 'f_micro', 'wf_micro'] else df.groupby(['filename', 'ns'])[metric].idxmin()
                 df_best = df.loc[index_best]
-                df_best['max_cov'] = df.reset_index('tau').loc[[ele[:-1] for ele in index_best]].groupby(level=['filename', 'ns'])['cov'].max()
-                dfs_best[metric] = df_best[columns + ["max_cov"]]
+                df_best['cov_max'] = df.reset_index('tau').loc[[ele[:-1] for ele in index_best]].groupby(level=['filename', 'ns'])['cov'].max()
+                df_best['wcov_max'] = df.reset_index('tau').loc[[ele[:-1] for ele in index_best]].groupby(level=['filename', 'ns'])['wcov'].max()
+                dfs_best[metric] = df_best
     else:
         logging.info("No predictions evaluated")
 
@@ -224,9 +234,11 @@ def write_results(df, dfs_best, out_dir='results', th_step=0.01):
     if not os.path.isdir(out_folder):
         os.makedirs(out_folder)
 
+    # Set the number of decimals to write in the output files based on the threshold step size
     decimals = int(np.ceil(-np.log10(th_step))) + 1
 
+    # ['n', 'tp', 'fp', 'fn', 'pr', 'rc', 'wn', 'wtp', 'wfp', 'wfn', 'wpr', 'wrc', 'cov', 'f', 'wcov', 'wf', 'mi', 'ru', 's']
     df.to_csv('{}/evaluation_all.tsv'.format(out_folder), float_format="%.{}f".format(decimals), sep="\t")
 
     for metric in dfs_best:
-        dfs_best[metric].to_csv('{}/evaluation_best_{}.tsv'.format(out_folder, metric), float_format="%.5f", sep="\t")
+        dfs_best[metric].to_csv('{}/evaluation_best_{}.tsv'.format(out_folder, metric), float_format="%.{}f".format(decimals), sep="\t")